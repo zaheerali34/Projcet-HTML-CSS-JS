2596ea30fa88640401457c0b92f339ce397f8d03
Zaheer Rahman <128778723+zaheerali34@users.noreply.github.com>
2023-07-30 02:12:05 -0700

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,22 @@
+let string = "";
+let buttons = document.querySelectorAll(".button");
+
+Array.from(buttons).forEach((btn)=> {
+    btn.addEventListener("click", (e)=> {
+        if (e.target.innerHTML == "=") {
+            string = eval(string);
+            document.querySelector(".input").value = string;
+        }
+
+        else if (e.target.innerHTML == "DEL") {
+            string = "";
+            document.querySelector(".input").value = string;
+        }
+
+        else {
+            string = string + e.target.innerHTML;
+            document.querySelector(".input").value = string;
+        }
+
+    })
+});
